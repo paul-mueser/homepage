a73e6f6fdbd8257256a42eb2699c89529c8b372d
Paul Müser <pmueser@mail.uni-paderborn.de>
2024-02-23 21:31:53 +0100

    add first draft for projectview and statsbot

---
+++ homepage/src/components/projects/StatsBot.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="project">
+        <h1>Discord Stats Bot</h1>
+        <p>
+            This is a project I have been working on for a while. It is a web application that allows users to create and manage their own projects.
+        </p>
+        <a class="highlight" href="#">More information</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'StatsBot',
+    }
+</script>
+
+<style scoped>
+</style>
--- homepage/src/views/ProjectView.vue
+++ homepage/src/views/ProjectView.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="container">
-    <h1>This is an about page</h1>
+    <div class="general">
+      <h1>General Informations</h1>
+      <p>
+        Here you can find a broad overview of my projects. For detailed information, please visit the project's page or the github repository.
+      </p>
+    </div>
+    <div class="projects">
+      <statsBot></statsBot>
+
+      <div class="project">
+        <h1>Project 2</h1>
+        <p>
+          This is a project I have been working on for a while. It is a web application that allows users to create and manage their own projects.
+        </p>
+        <a href="#">More information</a>
+      </div>
+      <div class="project">
+        <h1>Project 3</h1>
+        <p>
+          This is a project I have been working on for a while. It is a web application that allows users to create and manage their own projects.
+        </p>
+        <a href="#">More information</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import StatsBot from '@/components/projects/StatsBot.vue'
 export default {
   name: 'ProjectView',
   components: {
+    StatsBot
   }
 }
 </script>
