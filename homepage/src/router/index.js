13876c45d87fadb37823ab46d841a6722a6d55e4
Paul Müser <pmueser@mail.uni-paderborn.de>
2024-02-23 15:55:17 +0100

    update site routing

--- homepage/src/router/index.js
+++ homepage/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import OverviewView from '../views/OverviewView.vue'
 import ProjectView from '../views/ProjectView.vue'
 import LegalView from '../views/LegalView.vue'
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
