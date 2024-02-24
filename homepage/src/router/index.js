e9f6ebb18b1a0bdf9e356bedc9963bd6501a682c
Paul Müser <pmueser@mail.uni-paderborn.de>
2024-02-24 02:00:51 +0100

    update router

--- homepage/src/router/index.js
+++ homepage/src/router/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import OverviewView from '../views/OverviewView.vue'
 import ProjectView from '../views/ProjectView.vue'
 import StatsBotView from '../views/projects/StatsBotView.vue'
@@ -40,7 +40,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
+    history: createWebHashHistory(process.env.BASE_URL),
     routes
 })
 
