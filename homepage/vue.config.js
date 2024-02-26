9316e1e6488e9a94943bc0a31a08135d1fb5f0cb
Paul Müser <pmueser@mail.uni-paderborn.de>
2024-02-26 10:41:58 +0100

    add sitemap

--- homepage/vue.config.js
+++ homepage/vue.config.js
@@ -1,4 +1,30 @@
 const {defineConfig} = require('@vue/cli-service')
+
+const SitemapPlugin = require('sitemap-webpack-plugin').default
+
+const paths = [
+    '/',
+    '/projects/',
+    '/projects/statsbot/',
+    '/projects/musicbot/',
+]
+
 module.exports = defineConfig({
     transpileDependencies: true
 })
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new SitemapPlugin({
+                base: 'https://paulmueser.de',
+                paths,
+                options: {
+                    skipgzip: true
+                }
+            })
+        ]
+    },
+
+    
+}
