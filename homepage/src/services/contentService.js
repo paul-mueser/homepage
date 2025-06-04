cf1443820fa663e9192d05043148a12d20533ce3
Paul Müser <kontakt@paulmueser.de>
2025-06-04 17:41:26 +0200

    begin rework

--- homepage/src/services/contentService.js
+++ homepage/src/services/contentService.js
@@ -4,7 +4,7 @@ const API_URL = '{{API_URL}}';
 
 export const fetchProjects = async () => {
     const result = await axios.get(`${API_URL}/api/fetch-projects`);
-    for (project of result.data) {
+    for (const project of result.data) {
         const res = await axios.get(`${API_URL}/api/fetch-project-links`, { params: { projectname: project.name } });
         project.links = res.data;
     }
