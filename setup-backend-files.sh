b5e11cc3b79e5f8db0544985d2ab6439b60d440b
Paul Müser <kontakt@paulmueser.de>
2025-06-15 21:48:46 +0200

    fix pipeline

--- setup-backend-files.sh
+++ setup-backend-files.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sed -i "s|{{DB_HOST}}|${DB_HOST}|g" homepage/api/fetch-content.php
-sed -i "s|{{DB_USER}}|${DB_USER}|g" homepage/api/fetch-content.php
-sed -i "s|{{DB_PASS}}|${DB_PASSWORD}|g" homepage/api/fetch-content.php
-sed -i "s|{{DB_NAME}}|${DB_NAME}|g" homepage/api/fetch-content.php
+sed -i "s|{{DB_HOST}}|${DB_HOST}|g" homepage/api/fetch-project-links.php
+sed -i "s|{{DB_USER}}|${DB_USER}|g" homepage/api/fetch-project-links.php
+sed -i "s|{{DB_PASS}}|${DB_PASSWORD}|g" homepage/api/fetch-project-links.php
+sed -i "s|{{DB_NAME}}|${DB_NAME}|g" homepage/api/fetch-project-links.php
+
+sed -i "s|{{DB_HOST}}|${DB_HOST}|g" homepage/api/fetch-projects.php
+sed -i "s|{{DB_USER}}|${DB_USER}|g" homepage/api/fetch-projects.php
+sed -i "s|{{DB_PASS}}|${DB_PASSWORD}|g" homepage/api/fetch-projects.php
+sed -i "s|{{DB_NAME}}|${DB_NAME}|g" homepage/api/fetch-projects.php
