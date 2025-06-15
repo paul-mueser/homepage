c02d8955409b40e1d59d6f5b3cce9e253d8617dd
Paul Müser <kontakt@paulmueser.de>
2025-06-15 22:34:43 +0200

    update api

--- homepage/src/services/contentService.js
+++ homepage/src/services/contentService.js
@@ -5,14 +5,15 @@ const API_URL = '{{API_URL}}';
 export const fetchProjects = async () => {
     try {
         const result = await axios.get(`${API_URL}/api/fetch-projects`);
-        for (const project of result.data.data) {
+        const data = result.data.data || [];
+        for (const project of data) {
             try {
                 const res = await axios.post(`${API_URL}/api/fetch-project-links`, { projectName: project.name });
                 project.links = res.data.data;
             } catch (err) {
             }
         }
-        return result.data;
+        return data;
     } catch (err) {
         return [
         {
