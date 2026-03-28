81121a81887444c52441a50bcf22701c5772de81
Paul Müser <kontakt@paulmueser.de>
2026-03-28 14:11:10 +0100

    update backend secret handling

--- homepage/api/fetch-project-links.php
+++ homepage/api/fetch-project-links.php
@@ -1,10 +1,6 @@
 <?php
 header("Content-Type: application/json");
-
-$DB_HOST = '{{DB_HOST}}';
-$DB_USER = '{{DB_USER}}';
-$DB_PASS = '{{DB_PASS}}';
-$DB_NAME = '{{DB_NAME}}';
+include 'secrets.php';
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 
